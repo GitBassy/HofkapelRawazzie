7c7314c0e0ec7d69b05237180638e24e285ecd71
Reek, Bas van den <PS177983@summacollege.nl>
2019-10-02 17:44:54 +0000

    DB Import file to create the required database. First edition (V1.0)

---
+++ _FILES/blaaskapelrawazzie.sql
@@ -0,0 +1,169 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Gegenereerd op: 02 okt 2019 om 17:31
+-- Serverversie: 5.7.26
+-- PHP-versie: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `blaaskapelrawazzie`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+CREATE TABLE IF NOT EXISTS `event` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `eventtype_id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `content` varchar(800) NOT NULL,
+  `dueDate` date NOT NULL,
+  `duration` int(11) NOT NULL,
+  `created_at` date NOT NULL,
+  `updated_at` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `eventfile`
+--
+
+DROP TABLE IF EXISTS `eventfile`;
+CREATE TABLE IF NOT EXISTS `eventfile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_id` int(11) NOT NULL,
+  `name` int(11) NOT NULL,
+  `fileName` varchar(50) NOT NULL,
+  `path` varchar(250) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `eventtype`
+--
+
+DROP TABLE IF EXISTS `eventtype`;
+CREATE TABLE IF NOT EXISTS `eventtype` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `kapel_info`
+--
+
+DROP TABLE IF EXISTS `kapel_info`;
+CREATE TABLE IF NOT EXISTS `kapel_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `foundingDate` date NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `street` varchar(50) NOT NULL,
+  `buildingnumber` int(11) NOT NULL,
+  `buildingnumberAddition` varchar(3) DEFAULT NULL,
+  `phoneNumber` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `register`
+--
+
+DROP TABLE IF EXISTS `register`;
+CREATE TABLE IF NOT EXISTS `register` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `delay` int(11) NOT NULL,
+  `remark` varchar(250) DEFAULT NULL,
+  `created_at` date NOT NULL,
+  `updated_at` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `prefix` varchar(10) NOT NULL,
+  `surname` varchar(30) NOT NULL,
+  `birthday` date NOT NULL,
+  `phoneNumber` varchar(50) NOT NULL,
+  `userRole_id` int(11) NOT NULL,
+  `active` int(11) NOT NULL,
+  `created_at` date NOT NULL,
+  `updated_at` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `user-event`
+--
+
+DROP TABLE IF EXISTS `user-event`;
+CREATE TABLE IF NOT EXISTS `user-event` (
+  `user_id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `remark` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`event_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `userrole`
+--
+
+DROP TABLE IF EXISTS `userrole`;
+CREATE TABLE IF NOT EXISTS `userrole` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
