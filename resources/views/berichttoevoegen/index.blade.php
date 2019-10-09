056e61b7e3c1a227cad45b12c1227662b14c4b36
SumGitBash <ps177983@summacollege.nl>
2019-10-09 09:17:31 +0200

    First try to create new messages

--- resources/views/berichttoevoegen/index.blade.php
+++ resources/views/berichttoevoegen/index.blade.php
@@ -3,4 +3,44 @@
 
 @section('content')
 bericht toevoegen
+
+<div class="container">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    echo Form::model($user, array('route' => array('user.update', $user->id)))
+    
+    <form method="POST" action="/items/store">
+    @csrf
+    <table>
+        <caption>
+        <h1>Add new item</h1>
+        </caption>
+        <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Description</th>
+        </tr>
+        <tr>
+            
+        <td><input type="text" name="name" id="name"></td>
+        <td><input type="text" name="type" id="type"></td>
+        <td><input type="text" name="desc" id="desc"></td>
+
+        </tr>
+        
+    </table>  
+
+    <input type="submit" value="Submit">
+    </form>           
+    
+
+</div>
 @endsection
